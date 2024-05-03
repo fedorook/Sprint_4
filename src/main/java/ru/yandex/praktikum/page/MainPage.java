7cde3aeff07210c23841c5e4d54588cd011251c5
Fedorook <fedorook@gmail.com>
2024-05-04 01:01:24 +0800

    Replicating webinar till 2:00

--- src/main/java/ru/yandex/praktikum/page/MainPage.java
+++ src/main/java/ru/yandex/praktikum/page/MainPage.java
@@ -3,15 +3,53 @@ package ru.yandex.praktikum.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPage {
 private final WebDriver webDriver;
+private By orderStatusLocator = By.xpath("//button[text()='Статус заказа']");
+private By orderNumberInputLocator = By.xpath("//input[@placeholder='Введите номер заказа']");
+private By goButtonLocator = By.xpath("//button[text()='Go!']");
+private By notFoundImageLocator = By.xpath("//img[@alt='Not found']");
+private By createOrderButtonLocator = By.xpath("//div[contains(@class, 'Header')]//button[text()='Заказать']");
+
+
+
+
+
     public MainPage(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
 
     public void clickOrderStatusButton() {
-        WebElement orderStatusButton  = webDriver.findElement(By.xpath("//button[text()='Статус заказа']"));
+        WebElement orderStatusButton  = webDriver.findElement(orderStatusLocator);
         orderStatusButton.click();
+    }
+
+    public void enterOrderNumber(String orderNumber) {
+        WebElement orderInput = webDriver.findElement(orderNumberInputLocator);
+        orderInput.sendKeys(orderNumber);
+    }
+
+    public void clickGoButton() {
+        WebElement goButton = webDriver.findElement(goButtonLocator);
+        new WebDriverWait(webDriver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(goButton));
+        goButton.click();
+    }
+
+    public boolean notFoundImageIsDisplayed() {
+        WebElement notFoundImg = webDriver.findElement(notFoundImageLocator);
+        new WebDriverWait(webDriver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(notFoundImg));
+        return notFoundImg.isDisplayed();
+    }
+
+    public void clickCreateOrder() {
+        WebElement createOrderButton  = webDriver.findElement(createOrderButtonLocator);
+        createOrderButton.click();
     }
 }
--- src/main/java/ru/yandex/praktikum/page/OrderPage.java
+++ src/main/java/ru/yandex/praktikum/page/OrderPage.java
@@ -1,2 +1,80 @@
-package ru.yandex.praktikum.page;public class OrderPage {
+package ru.yandex.praktikum.page;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class OrderPage {
+    private final WebDriver webDriver;
+    private final By nameInputLocator = By.xpath("//input[@placeholder='* Имя']");
+    private final By lastnameInputLocator = By.xpath("//input[@placeholder='* Фамилия']");
+    private final By addressInputLocator = By.xpath("//input[@placeholder='* Адрес: куда привезти заказ']");
+    private final By subwayInputLocator = By.xpath("//input[@placeholder='* Станция метро']");
+    private final String stationMenuItemLocator = "//div[text()='%s']";
+    private final By phoneInputLocator = By.xpath("//input[@placeholder='* Телефон: на него позвонит курьер']");
+    private final By nextButtonLocator = By.xpath("//button[text()='Далее']");
+    private final By dateInputLocator = By.xpath("//input[@placeholder='* Когда привезти самокат']");
+    private final By rentPeriodInputLocator = By.xpath("//div[text()='* Срок аренды']");
+    private final By rentPeriodMenuItemLocator = By.xpath("//div[text()='трое суток']");
+    private final By createOrderButtonLocator = By.xpath("//button[text()='Заказать']");
+    private final By yesButtonLocator = By.xpath("//button[(text()='Да')]");
+
+
+
+
+    public OrderPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void fillCustomerInfo(String name, String lastname, String address, String subwayTitle, String phone) {
+        WebElement nameInput = webDriver.findElement(nameInputLocator);
+        nameInput.sendKeys(name);
+
+        WebElement lastnameInput = webDriver.findElement(lastnameInputLocator);
+        lastnameInput.sendKeys(lastname);
+
+        WebElement addressInput = webDriver.findElement(addressInputLocator);
+        addressInput.sendKeys(address);
+
+        WebElement subwayInput = webDriver.findElement(subwayInputLocator);
+        subwayInput.click();
+
+        WebElement arbatskayaStationMenu = webDriver.findElement(By.xpath(String.format(stationMenuItemLocator, subwayTitle)));
+        ((JavascriptExecutor) webDriver).executeScript("arguments[0].scrollIntoView();", arbatskayaStationMenu);
+        arbatskayaStationMenu.click();
+
+
+        WebElement phoneInput = webDriver.findElement(phoneInputLocator);
+        phoneInput.sendKeys(phone);
+    }
+
+    public void clickNextButton() {
+        WebElement nextButton  = webDriver.findElement(nextButtonLocator);
+        nextButton.click();
+    }
+
+    public void fillRentInfo(String date) {
+        WebElement dateInput = webDriver.findElement(dateInputLocator);
+        dateInput.sendKeys(date, Keys.ENTER);
+
+        WebElement rentPeriodInput = webDriver.findElement(rentPeriodInputLocator);
+        rentPeriodInput.click();
+
+        WebElement rentPeriodMenuItem = webDriver.findElement(rentPeriodMenuItemLocator);
+        rentPeriodMenuItem.click();
+    }
+
+    public void clickCreateOrderButton() {
+        WebElement createOrderButton = webDriver.findElement(createOrderButtonLocator);
+        createOrderButton.click();
+    }
+
+//    public void clickYesButton() {
+//        WebElement yesButton = webDriver.findElement(yesButtonLocator);
+//        new WebDriverWait(webDriver, Duration.ofSeconds(5))
+//                .until(ExpectedConditions.elementToBeClickable(yesButton));
+//        yesButton.click();
+//    }
 }
